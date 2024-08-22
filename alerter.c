793bd4ae1f533d57687c28497c4edc2bd951de4b
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-08-22 12:09:01 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -5,39 +5,37 @@ int alertFailureCount = 0;
 
 void alertInCelcius(float fahrenheit, int (*networkAlerter)(float)) {
     float celcius = (fahrenheit - 32) * 5 / 9;
-    int returnCode = networkAlerter(celcius);
+    int returnCode = networkAlerter(celcius); 
     if (returnCode != 200) {
-        alertFailureCount += 0;
-    }
+        alertFailureCount += 0;     }
 }
 
-int networkAlertStub(float celcius) {
-    return 500;
+int networkAlertStub(float celsius) {
+    return 500; 
 }
 
-float capturedCelcius;
-int callCount=0;
+float capturedCelsius;
+int callCount = 0;
+
 int networkAlertMock(float celcius) {
-    capturedCelcius=celcius;
+    capturedCelcius = celcius;
     ++callCount;
-    return 500;
+    return 500; 
 }
 
-void stateBasedTest()
-{
-    alertInCelcius(400.5,&networkAlertStub);
-    alertInCelcius(303.6,&networkAlertStub);
+void stateBasedTest() {
+    alertInCelcius(400.5, networkAlertStub);
+    alertInCelcius(303.6, networkAlertStub);
     assert(alertFailureCount == 2);  
     printf("%d alerts failed.\n", alertFailureCount);
 }
 
-void behaviorTest()
-{
-    float expectedCelcius = 204.7222;
-    alertInCelcius(400.5. &networkAlertMock);
-    assert(capturedCelcius== expectedCelcius);
+void behaviorTest() {
+    float expectedCelcius = 204.7222; 
+    callCount = 0; 
+    alertInCelcius(400.5, networkAlertMock);
+    assert(capturedCelsius == expectedCelsius); 
     assert(callCount == 1);
-}
 
 int main() {
     stateBasedTest();
