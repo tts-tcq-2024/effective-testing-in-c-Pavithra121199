a5c9a2e0e7b948ba28ba7af782099b04bb0cd52d
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-08-22 12:04:32 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -3,32 +3,45 @@
 
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 500;
-}
-
-void alertInCelcius(float farenheit) {
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
+void alertInCelcius(float fahrenheit, int (*networkAlerter)(float)) {
+    float celcius = (fahrenheit - 32) * 5 / 9;
+    int returnCode = networkAlerter(celcius);
     if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
 }
 
+int networkAlertStub(float celcius) {
+    return 500;
+}
+
+float capturedCelcius;
+int callCount=0;
+int networkAlertMock(float celcius) {
+    capturedCelcius=celcius;
+    ++callCount;
+    return 500;
+}
+
+void stateBasedTest()
+{
+    alertInCelcius(400.5,&networkAlertStub);
+    alertInCelcius(303.6,&networkAlertStub);
+    assert(alertFailureCount == 2);  
+    printf("%d alerts failed.\n", alertFailureCount);
+}
+
+void behaviorTest()
+{
+    float expectedCelcius = 204.7222;
+    alertInCelcius(400.5. &networkAlertMock);
+    assert(capturedCelcius== expectedCelcius);
+    assert(callCount == 1);
+}
 
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    assert(alertFailureCount == 2); 
-    printf("%d alerts failed.\n", alertFailureCount);
+    stateBasedTest();
+    behaviorTest();
     printf("All is well (maybe!)\n");
     return 0;
 }
